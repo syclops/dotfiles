6b2e1802c9713c441d49dedf56e1e3bc05401751
Steve Matsumoto <smatsumoto90@gmail.com>
2015-10-21 23:56:04 +0200

    Add configuration commands script

---
+++ mint/config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+set -o errexit
+
+# Git settings
+git config --global user.name "Steve Matsumoto"
+echo "Enter your GMail address below:"
+read email
+git config --global user.email "$email"
+git config --global push.default simple
