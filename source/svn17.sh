dc28928b8ae92b5b677eda0685b001f4dfc5d155
Steve Matsumoto <smatsumoto90@gmail.com>
2015-10-23 01:50:01 +0200

    Add svn 1.7 setup script

---
+++ source/svn17.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o errexit
+
+wget https://archive.apache.org/dist/subversion/subversion-1.7.22.tar.gz
+tar xvzf subversion-1.7.22.tar.gz
+cd subversion-1.7.22
+./configure --prefix=$HOME/dotfiles/local
+make
+make install
+rm -rf subversion-1.7.22
+rm subversion-1.7.22.tar.gz
