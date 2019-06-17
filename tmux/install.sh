fd392d303a7bef593db12bf895321470e662a829
Steve Matsumoto <steve@stevematsumoto.net>
2019-06-17 13:57:11 -0400

    Add more tmux files

---
+++ tmux/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+set -o errexit
+
+ln -s tmux.conf ~/.tmux.conf
+
+if [ -z "$XDG_CONFIG_HOME" ]; then
+  ln -s . "$HOME/.config/tmux"
+else
+  ln -s . "$XDG_CONFIG_HOME/tmux"
+fi
+
