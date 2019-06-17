35b5d5f2a8dab5e8c9d7b2c8e24becc6764f81fd
Steve Matsumoto <smatsumoto90@westmoreland.dreamhost.com>
2019-06-17 13:00:24 -0700

    Fix minor bugs in tmux config

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 ln -s tmux.conf ~/.tmux.conf
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 if [ -z "$XDG_CONFIG_HOME" ]; then
   mkdir -p "$HOME/.config"
   ln -s "$(pwd)" "$HOME/.config/tmux"
